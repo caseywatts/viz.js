276319e0ade6ac3edd91858e211da15774af80ad
Michael Daines <mdaines@fastmail.com>
2015-10-28 19:14:48 -0400

    Add experimental png-image-element format. (#48)

--- src/post.js
+++ src/post.js
@@ -1,10 +1,6 @@
   var graphviz;
   
-  function Viz(src) {
-    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
-    var format = options.format === undefined ? "svg" : options.format;
-    var engine = options.engine === undefined ? "dot" : options.engine;
-    
+  function render(src, format, engine) {
     if (typeof graphviz === "undefined") {
       graphviz = Module();
     }
@@ -20,6 +16,38 @@
     }
     
     return resultString;
+  }
+  
+  function svgXmlToPngImage(svgXml) {
+    var svgImage = new Image();
+    svgImage.src = "data:image/svg+xml;utf8," + svgXml;
+
+    var pngImage = new Image();
+
+    svgImage.onload = function() {
+      var canvas = document.createElement("canvas");
+      canvas.width = svgImage.width;
+      canvas.height = svgImage.height;
+
+      var context = canvas.getContext("2d");
+      context.drawImage(svgImage, 0, 0);
+
+      pngImage.src = canvas.toDataURL("image/png");
+    }
+    
+    return pngImage;
+  }
+  
+  function Viz(src) {
+    var options = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+    var format = options.format === undefined ? "svg" : options.format;
+    var engine = options.engine === undefined ? "dot" : options.engine;
+    
+    if (format == "png-image-element") {
+      return svgXmlToPngImage(render(src, "svg", engine));
+    } else {
+      return render(src, format, engine);
+    }
   }
   
   if (typeof module === "object" && module.exports) {
